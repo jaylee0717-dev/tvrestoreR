50bf673e9ff386199845e6cc71ac033ab03cbf07
Jay Lee <jaylee0717@tamu.edu>
2025-11-04 11:19:41 -0600

    Add sketch of generate_example_image

---
+++ R/Simulations.R
@@ -0,0 +1,13 @@
+generate_example_image <- function(size = c(64, 64), pattern = "gradient",
+                                   noise_sigma = 0.0, missing_fraction = 0.0,
+                                   mask_type = "disk", seed = NULL){
+  # Set seed
+
+  # Generate base image
+
+  # Add noise
+
+  # Generate Mask
+
+  # Return Original image, Corrupted image, and mask pixels
+}
