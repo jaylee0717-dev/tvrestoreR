49fab108b3cfe444bd36e67cc14335902944a2d0
Jay Lee <jaylee0717@tamu.edu>
2025-11-06 09:54:03 -0600

    Add Original image generation

--- R/Simulations.R
+++ R/Simulations.R
@@ -2,9 +2,22 @@ generate_example_image <- function(size = c(64, 64), pattern = "gradient",
                                    noise_sigma = 0.0, missing_fraction = 0.0,
                                    mask_type = "disk", seed = NULL){
   # Set seed
-
+  if (!is.null(seed)) set.seed(seed)
+  nr <- size[1]
+  nc <- size[2]
   # Generate base image
-
+  if (pattern == "disk"){
+    x <- seq(-1,1,length.out=nc)
+    y <- seq(-1,1,length.out=nr)
+    grid <- outer(y, x, FUN = function(yy, xx) sqrt(xx^2 + yy^2))
+    orig <- ifelse(grid <= 0.8, 1, 0)
+  }
+  else if (pattern == "gradient"){
+    orig <- outer(seq(0,1,length.out=nr), seq(0,1,length.out=nc), FUN = function(x, y) x)
+  }
+  else if (pattern == "checkerboard") {
+    orig <- matrix(rep(rep(c(0,1), length.out = nc), length.out = nr),
+                   nrow = nr, byrow = TRUE)
   # Add noise
 
   # Generate Mask
