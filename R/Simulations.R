077b8aef5ace894e26960d085d4e9e0723fb4dbf
Jay Lee <jaylee0717@tamu.edu>
2025-11-18 21:36:12 -0600

    Change the example such that it is grayscale

--- R/Simulations.R
+++ R/Simulations.R
@@ -18,7 +18,7 @@
 #'
 #' @examples
 #' ex <- generate_example_image(missing_fraction = 0.4, noise_sigma = 0.2)
-#' image(ex$corrupted, main = "Example corrupted image")
+#' image(ex$corrupted, main = "Example corrupted image", col=grey(seq(0,1,length.out = 256)))
 generate_example_image <- function(size = c(64, 64), pattern = "gradient",
                                    noise_sigma = 0.0, missing_fraction = 0.0,
                                    mask_type = "disk", seed = NULL){
