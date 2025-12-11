771177719d6022fbd5a0560e0bf8107097eb40a5
Jay Lee <jaylee0717@tamu.edu>
2025-12-11 17:12:43 -0600

    Add D and Fmat as a passable parameter to avoid repetitive computation

--- R/restorer.R
+++ R/restorer.R
@@ -1,12 +1,14 @@
-#' Title
+#' tv_restore
 #'
 #' @param img
 #' @param mask
 #' @param lambda
 #' @param method
-#' @param tau
-#' @param sigma
-#' @param theta
+#' @param task
+#' @param u0 Initialized image. Defaults to an entirely black image.
+#' @param max_iter Maximum iteration. Default 10000.
+#' @param tol Tolerance on the 2-norm difference of primal and dual variables. Default 1e-5.
+#' @param verbose Controls if detailed printouts on the looped variables will be provided. Default False.
 #'
 #' @returns
 #' @export
@@ -14,7 +16,9 @@
 #' @examplesa
 tv_restore <- function(img, mask = NULL, lambda,
                        method = "primal_dual", task = "ROF_inpainting", u0 = NULL,
-                       max_iter = 10000, tol = 1e-5, verbose = FALSE){
+                       max_iter = 10000, tol = 1e-4, verbose = FALSE,
+                       # Primal-Dual Parameters
+                       D= NULL, Fmat= NULL, ...){
   ### Input Checks
   if (!is.matrix(img)) stop("`img` must be a 2D numeric matrix.")
   nr <- nrow(img); nc <- ncol(img)
@@ -25,7 +29,7 @@ tv_restore <- function(img, mask = NULL, lambda,
     stop("mask dimensions must match img.")
   }
   if (is.null(u0)){
-    u0 <- as.numeric(ifelse(is.na(img), 0, img))
+    u0 <- as.numeric(ifelse(is.na(img), 0.5, img))
   }
   ###################MORE TO ADD LATER ###################
 
@@ -40,12 +44,15 @@ tv_restore <- function(img, mask = NULL, lambda,
     mask_vec <- as.numeric(as.vector(mask))   # as.vector keeps column-major order
 
     # Solve saddle point
-    res_vec <- find_saddle_point(im_vec = im_vec, mask_vec = mask_vec,
+    res_vec <- find_primaldual_saddle_point(im_vec = im_vec, mask_vec = mask_vec,
                       task = task, lmda = lambda, u0 = u0,
-                      tol = tol, max_iter = max_iter, verbose = verbose)
+                      tol = tol, max_iter = max_iter, verbose = verbose,
+                      D = D, Fmat = Fmat, ...)
     # Return as matrix, strictly capped between 0 and 1
     res <- matrix(res_vec, nrow = nr, ncol = nc)   # column-major fill
     res[res < 0] <- 0; res[res > 1] <- 1;
     return(res)
   }
+
 }
+
