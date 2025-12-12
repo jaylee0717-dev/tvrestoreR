8d39e83ce36c2ad8107bbe40b1e33a6c83b86676
Jay Lee <jaylee0717@tamu.edu>
2025-12-11 18:43:09 -0600

    Add more input checks, add split_bregman case

--- R/restorer.R
+++ R/restorer.R
@@ -13,46 +13,71 @@
 #' @returns
 #' @export
 #'
-#' @examplesa
+#' @examples
 tv_restore <- function(img, mask = NULL, lambda,
                        method = "primal_dual", task = "ROF_inpainting", u0 = NULL,
                        max_iter = 10000, tol = 1e-4, verbose = FALSE,
-                       # Primal-Dual Parameters
-                       D= NULL, Fmat= NULL, ...){
+                       # Task-specific parameters here
+                       ...){
   ### Input Checks
-  if (!is.matrix(img)) stop("`img` must be a 2D numeric matrix.")
-  nr <- nrow(img); nc <- ncol(img)
-  if (is.null(mask)) {
-    mask <- !is.na(img)
-  }
-  if (!all(dim(mask) == c(nr, nc))) {
-    stop("mask dimensions must match img.")
-  }
-  if (is.null(u0)){
-    u0 <- as.numeric(ifelse(is.na(img), 0.5, img))
-  }
-  ###################MORE TO ADD LATER ###################
-
   # Match method string
   method <- match.arg(method, choices = c("primal_dual", "split_bregman"))
   task <- match.arg(task, choices = c("denoising", "inpainting", "ROF_inpainting"))
 
-  # Call corresponding restore function
-  if (method == "primal_dual") {
-    # Vectorize
-    im_vec <- as.numeric(ifelse(is.na(img), 0, img))   # length n^2
-    mask_vec <- as.numeric(as.vector(mask))   # as.vector keeps column-major order
 
+  if (!is.matrix(img)) stop("`img` must be a 2D numeric matrix.")
+  nr <- nrow(img); nc <- ncol(img)
+
+  # Check Mask
+  if (task == "denoising") {
+    if (!is.null(mask) && verbose) message("Note: Mask ignored for 'denoising' task.")
+    mask <- matrix(1, nrow = nrow(img), ncol = ncol(img))
+  } else {
+    if (is.null(mask)) {
+      if (any(is.na(img))) {
+        mask <- !is.na(img)
+        if (verbose) message("Auto-detected mask from NA values.")
+      } else {
+        stop(sprintf("Task '%s' requires a 'mask' argument.", task))
+      }
+    }
+  }
+  if (!all(dim(mask) == c(nr, nc))) {
+    stop("mask dimensions must match img.")
+  }
+
+  # Initialize u0
+  if (is.null(u0)){
+    u0 <- as.numeric(ifelse(is.na(img), 0.5, img))
+  }
+
+  # Replace NAs in image with 0, vectorize inputs for efficiency
+  im_vec <- as.numeric(ifelse(is.na(img_clean), 0, img_clean))   # length n^2
+  mask_vec <- as.numeric(as.vector(mask))   # as.vector keeps column-major order
+
+
+  ## Pre-Compute Large Operators if not provided
+  args <- list(...)
+  D_op <- if(is.null(args$D)) compute_D(n) else args$D
+  F_op <- if(is.null(args$Fmat) && method == "primal_dual") compute_F(n) else args$Fmat
+
+
+  #### Call corresponding restore function
+  if (verbose) message(sprintf("Running %s using %s...", task, method))
+  if (method == "primal_dual") {
     # Solve saddle point
     res_vec <- find_primaldual_saddle_point(im_vec = im_vec, mask_vec = mask_vec,
                       task = task, lmda = lambda, u0 = u0,
                       tol = tol, max_iter = max_iter, verbose = verbose,
-                      D = D, Fmat = Fmat, ...)
-    # Return as matrix, strictly capped between 0 and 1
-    res <- matrix(res_vec, nrow = nr, ncol = nc)   # column-major fill
-    res[res < 0] <- 0; res[res > 1] <- 1;
-    return(res)
+                      D = D_op, Fmat = F_op, ...)
   }
-
+  else if (method == "split_bregman") {
+    res_vec <- find_split_bregman(im_vec, mask_vec, task, lambda, u0 = u0,
+                                  tol = tol, max_iter = max_iter, verbose = verbose,
+                                  D = D_op, ...)
+  }
+  # Return as matrix, strictly capped between 0 and 1
+  res <- matrix(res_vec, nrow = nr, ncol = nc)   # column-major fill
+  res[res < 0] <- 0; res[res > 1] <- 1;
+  return(res)
 }
-
