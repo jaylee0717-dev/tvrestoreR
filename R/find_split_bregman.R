498734da9197b8c634433220778f14422c01bd08
Jay Lee <jaylee0717@tamu.edu>
2025-12-11 19:28:21 -0600

    Modify the implementation of inpainting to have hugh fidelity at mask

--- R/find_split_bregman.R
+++ R/find_split_bregman.R
@@ -1,4 +1,4 @@
-#' Title
+#' find_split_bregman
 #'
 #' @param im_vec
 #' @param mask_vec
@@ -12,9 +12,7 @@
 #' @param D
 #'
 #' @returns
-#' @export
-#'
-#' @examples
+#' @noRd
 find_split_bregman <- function(im_vec, mask_vec, task,
                                lmda, u0, mu = 1.0,
                                tol = 1e-4, max_iter = 500, verbose = FALSE,
@@ -29,15 +27,23 @@ find_split_bregman <- function(im_vec, mask_vec, task,
     D <- compute_D(n)
   }
 
+  # --- 1. DETERMINE WEIGHTS & THRESHOLDS ---
+  if (task == "inpainting") {
+    fidelity_weight <- 10000.0 # Huge penalty to anchor known pixels
+    shrink_tau <- 1.0 / mu
+
+  } else {
+    fidelity_weight <- 1.0
+    shrink_tau <- 1.0 / (lmda * mu)
+  }
+
   # Compute Laplacian (D^T D) once
   Laplacian <- crossprod(D)
 
-  # Build Linear System according to task (with stability padding)
-  if (task == "inpainting") {
-    LHS <- mu * Laplacian + Diagonal(n=length(im_vec), x=1e-10)
-  } else { # Soft constraints (Denoising / ROF_inpainting)
-    LHS <- Diagonal(x = mask_vec) + mu * Laplacian
-  }
+  # Build LHS Matrix
+  Fidelity <- Diagonal(x = (mask_vec * fidelity_weight))
+  LHS <- Fidelity + (mu * Laplacian) + Diagonal(n = n2, x = 1e-10)
+
   solver_chol <- Cholesky(LHS)
 
   ## Initialization
@@ -52,20 +58,18 @@ find_split_bregman <- function(im_vec, mask_vec, task,
     u_old <- u
 
     # update u
-    rhs_data <- mask_vec * im_vec
+    rhs_data <- (mask_vec * fidelity_weight * im_vec)
     rhs_reg  <- mu * as.numeric(crossprod(D, d - b))
-    RHS <- rhs_data + rhs_reg
     # Solve Au = b
-    u <- as.numeric(solve(solver_chol, RHS))
+    u <- as.numeric(solve(solver_chol, rhs_data + rhs_reg))
     if (task == "inpainting") {
       # Reset known pixels to original values
       u[mask_vec == 1] <- im_vec[mask_vec == 1]
     }
 
     # update d
-    # d = shrink(Du + b, lambda / mu)
     Du <- as.numeric(D %*% u)
-    d <- shrink(Du + b, tau = (lmda / mu))
+    d <- shrink(Du + b, tau = shrink_tau)
 
     # update b
     b <- b + (Du - d)
@@ -76,7 +80,7 @@ find_split_bregman <- function(im_vec, mask_vec, task,
     if (count > max_iter) break
 
     rel_change <- vecnorm2(u - u_old) / (vecnorm2(u) + 1e-10)
-    if (rel_change < tol) break
+    if (rel_change < tol && count > 5) break
 
 
     ## Output Control
@@ -85,5 +89,6 @@ find_split_bregman <- function(im_vec, mask_vec, task,
     }
   }
 
+  if (verbose) cat("Total iterations:", count, "\n")
   return(u)
 }
